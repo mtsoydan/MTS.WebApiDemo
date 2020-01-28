df5b2dd2d48404b83c93f2c2ce34421787bc283d
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-29 00:42:54 +0300

    Entities Klasörü Oluşturuldu

---
+++ MTS.WebApiDemo/MTS.WebApiDemo/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTS.WebApiDemo.Entities
+{
+    public class Product :IEntity
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryID{ get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitInStock { get; set; }
+    }
+}
