f9dfb3086aed192555cedacc09e41b14ad2a6387
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-29 01:20:59 +0300

    Repository Tasarım Kalıbı Uygulandı

---
+++ MTS.WebApiDemo/MTS.WebApiDemo/DataAcces/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTS.WebApiDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTS.WebApiDemo.DataAcces
+{
+    public class NorthwindContext  :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-PKLDNBN;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
