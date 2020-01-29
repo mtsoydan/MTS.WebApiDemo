16d00da0deba4f2f72253844bb5cb8636127b25c
mtsoydan <mustafataha.soydan@gmail.com>
2020-01-29 21:24:28 +0300

    ContactModel olusturuldu.

---
+++ MTS.WebApiDemo/MTS.WebApiDemo/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MTS.WebApiDemo.Models;
+
+namespace MTS.WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        [HttpGet("")]
+       public List<ContactModel> Contacts()
+        {
+            return new List<ContactModel>
+            {
+                new ContactModel{ContactID =1,FirstName="mts",LastName="Soydan"},
+                new ContactModel{ContactID=2 ,LastName="soydan",FirstName="furkan"}
+            };
+        }
+    }
+}
